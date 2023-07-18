6199f617eaaad47218b6994414c842efb1e2c68b
Philip Witty <philip.witty@gmail.com>
2023-07-18 19:20:21 +0200

    Compile allow and deny list as globs to allow wildcards
    * as the glob special characters are not valid package names this should not cause compile errors for any matches of existing modules

--- cmd/depguard/main_test.go
+++ cmd/depguard/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"io/fs"
 	"testing"
 
 	"github.com/OpenPeeDeeP/depguard/v2"
@@ -16,6 +17,7 @@ var expectedConfigStruct = &depguard.LinterSettings{
 		Files: []string{"$all", "!$test"},
 		Allow: []string{"$gostd", "github.com/"},
 		Deny: map[string]string{
+			"github.com/**/test":     "No test packages allowed",
 			"reflect":                "Who needs reflection",
 			"github.com/OpenPeeDeeP": "Use Something Else",
 		},
@@ -29,50 +31,45 @@ var expectedConfigStruct = &depguard.LinterSettings{
 	},
 }
 
-func TestJsonConfigurator(t *testing.T) {
-	con := &jsonConfigurator{}
-	f, err := testfiles.Open("testfiles/.depguard.json")
-	if err != nil {
-		t.Fatal("could not read embedded file")
+func TestConfigurators(t *testing.T) {
+	mustGetFile := func(name string) fs.File {
+		f, err := testfiles.Open(name)
+		if err != nil {
+			t.Fatal("could not read embedded file")
+		}
+		return f
 	}
-	set, err := con.parse(f)
-	if err != nil {
-		t.Fatalf("file is not a valid json file: %s", err)
+	cases := []struct {
+		name         string
+		inputFile    fs.File
+		configurator configurator
+	}{
+		{
+			name:         "json",
+			inputFile:    mustGetFile("testfiles/.depguard.json"),
+			configurator: &jsonConfigurator{},
+		},
+		{
+			name:         "yaml",
+			inputFile:    mustGetFile("testfiles/.depguard.yaml"),
+			configurator: &yamlConfigurator{},
+		},
+		{
+			name:         "toml",
+			inputFile:    mustGetFile("testfiles/.depguard.toml"),
+			configurator: &tomlConfigurator{},
+		},
 	}
-	diff := cmp.Diff(expectedConfigStruct, set)
-	if diff != "" {
-		t.Errorf("did not create expected config\n%s", diff)
-	}
-}
-
-func TestYamlConfigurator(t *testing.T) {
-	con := &yamlConfigurator{}
-	f, err := testfiles.Open("testfiles/.depguard.yaml")
-	if err != nil {
-		t.Fatal("could not read embedded file")
-	}
-	set, err := con.parse(f)
-	if err != nil {
-		t.Fatalf("file is not a valid yaml file: %s", err)
-	}
-	diff := cmp.Diff(expectedConfigStruct, set)
-	if diff != "" {
-		t.Errorf("did not create expected config\n%s", diff)
-	}
-}
-
-func TestTomlConfigurator(t *testing.T) {
-	con := &tomlConfigurator{}
-	f, err := testfiles.Open("testfiles/.depguard.toml")
-	if err != nil {
-		t.Fatal("could not read embedded file")
-	}
-	set, err := con.parse(f)
-	if err != nil {
-		t.Fatalf("file is not a valid toml file: %s", err)
-	}
-	diff := cmp.Diff(expectedConfigStruct, set)
-	if diff != "" {
-		t.Errorf("did not create expected config\n%s", diff)
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			set, err := c.configurator.parse(c.inputFile)
+			if err != nil {
+				t.Fatalf("file is not a valid json file: %s", err)
+			}
+			diff := cmp.Diff(expectedConfigStruct, set)
+			if diff != "" {
+				t.Errorf("did not create expected config\n%s", diff)
+			}
+		})
 	}
 }
